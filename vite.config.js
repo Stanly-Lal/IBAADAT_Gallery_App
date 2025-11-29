b4cb60a483ad56a955569d60943a1bf126db4fc0
Stanly Lal <stanlylal17@gmail.com>
2025-11-30 00:49:14 +0530

    fixed custom domain error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: '/IBAADAT_Gallery_App/',
+  base: "/",
   plugins: [react()],
-})
+});
